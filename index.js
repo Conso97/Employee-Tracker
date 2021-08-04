94a992d794af00478d36ce14ac392805fc9cc449
Consolata Njeri <conso4u@gmail.com>
2021-08-04 13:14:51 -0700

    working solution

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const db = mysql.createConnection(
     }
 );
 
+// options of command line actions
 const userActions = [
     {
         type: "list",
@@ -31,42 +32,33 @@ const userActions = [
     }
 ];
 
-const departmentQuestions = [
-    {
-        type: "input",
-        message: "What is the name of the department?",
-        name: "departmentName" 
-    }
-];
+function viewEmployees() {
 
-const roleQuestions = [
-    {
-        type: "input",
-        message: "What is the name of the role?",
-        name: "roleName" 
-    },
-    {
-        type: "input",
-        message: "What is the salary of the role?",
-        name: "roleSalary" 
-    }
-];
+    db.query(`SELECT * FROM employees `, function(err, response) {
 
-const employeeQuestions = [
-    {
-        type: "input",
-        message: "What is the employee's first name?",
-        name: "firstName" 
-    },
-    {
-        type: "input",
-        message: "What is the employee's last name?",
-        name: "lastName" 
-    },
-];
+        console.table(response);
+        return askUserForAction();
+    });
+}
 
+function viewAllRoles() {
+    db.query(`SELECT * FROM roles`, function(err, response) {
 
-function askUserForAction() {
+        console.table(response);
+        return askUserForAction();
+    });
+}
+
+function viewAllDepartments() {
+    db.query(`SELECT * FROM departments`, function(err, response) {
+
+        console.table(response);
+        return askUserForAction();
+    });
+    
+}
+
+const askUserForAction = () => {
     return inquirer
     .prompt(userActions)
     .then((response) => {
@@ -88,29 +80,115 @@ function askUserForAction() {
             case "Add Department":
                 return createDepartment();
             case "Quit":
-                return;
+                console.log("Exiting...");
+                process.exit(0);
             default:
                 console.log("Requested Action not recognized");
         }
     });
 }
 
-function viewEmployees() {
+const roleQuestions = [
+    {
+        type: "input",
+        message: "What is the name of the role?",
+        name: "roleName" 
+    },
+    {
+        type: "input",
+        message: "What is the salary of the role?",
+        name: "roleSalary" 
+    }
+];
 
-    db.query(`SELECT * FROM employees `, function(err, response) {
+function createRole() {
+    //Get all existing departments 
+    db.query(`SELECT * FROM departments`, function(err, response) {
 
-        console.table(response);
-        return askUserForAction();
+        var depts = []; 
+        for (var i = 0; i < response.length; i++) {
+            depts.push(response[i].name); // save department names
+        }
+
+        var roleDept = [{
+            type: "list",
+            message: "Which department does the role belong to?",
+            choices: depts,
+            name: "departmentForRole",
+        }];
+
+        return inquirer
+        .prompt(roleQuestions.concat(roleDept))
+        .then((roleResponse) => {
+            // add role details to the DB
+            var dbQuery =  `INSERT INTO roles (title, department, salary) VALUES (?, ?, ?)`;
+            db.query(dbQuery, 
+                [roleResponse.roleName, roleResponse.departmentForRole, roleResponse.roleSalary]
+                , function(err, res) {
+
+                return askUserForAction();
+            });
+        });
     });
 }
 
+const departmentQuestions = [
+    {
+        type: "input",
+        message: "What is the name of the department?",
+        name: "departmentName" 
+    }
+];
+
+function createDepartment() {
+
+    //'inquirer.prompt' for department info 
+    return inquirer
+    .prompt(departmentQuestions)
+    .then((response) => {
+
+        var name = response.departmentName;
+
+        //Add new department to database
+        var dbQuery =  `INSERT INTO departments (name) VALUES (?)`;
+        db.query(dbQuery, name, function(err, response) {
+
+            if (err !== null) {
+                console.log("Caught error: " + String(err));
+                return;
+            }
+
+            return askUserForAction();
+        });
+
+    });
+}
+
+const employeeQuestions = [
+    {
+        type: "input",
+        message: "What is the employee's first name?",
+        name: "firstName" 
+    },
+    {
+        type: "input",
+        message: "What is the employee's last name?",
+        name: "lastName" 
+    },
+];
+
 function createEmployee() {
+    // Get existing roles to display in command options
     db.query(`SELECT * FROM roles`, function(err, response) {
 
-        console.table(response);
+        if (err !== null) {
+            console.log("Caught error: " + String(err));
+            return;
+        }
+
         var titles = [];
         for (var i = 0; i < response.length; i++) {
-            titles.push(response[i].title);
+            titles.push(response[i].title); // select role titles
         }
         
         var employeeDetails = [
@@ -140,7 +218,7 @@ function createEmployee() {
         .prompt(employeeQuestions.concat(employeeDetails))
         .then((empResponse) => {
             
-            // get role details 
+            // get selected role details 
             var empRole = null;
 
             for (var i = 0; i < response.length; i++) {
@@ -149,15 +227,24 @@ function createEmployee() {
                 }
             }
 
+            employeeManager = empResponse.employeeManager !== 'None' ? empResponse.employeeManager : null;
+            
+            // Add new employee with role information to database
             var dbQuery = `INSERT INTO employees (first_name, last_name, title, department, salary, manager) VALUES (?, ?, ?, ?, ?, ?)`;
             db.query(dbQuery, 
                 [
                     empResponse.firstName, empResponse.lastName, empResponse.employeeRole, 
-                    empRole.department, empRole.salary, empResponse.employeeManager
+                    empRole.department, empRole.salary, employeeManager
                 ]
                 , function(err, response) {
+                
+                    if (err !== null) {
+                        console.log("Caught error: " + String(err));
+                        return;
+                    }
 
-                return askUserForAction();
+                    console.log(`Added ${empResponse.firstName} ${empResponse.lastName} to database`);
+                    return askUserForAction();
             });
         });
     });
@@ -165,15 +252,28 @@ function createEmployee() {
 
 function updateEmployeeRole() {
 
-    db.query(`SELECT * FROM employees`, function(err, response) {
+    // Get existing employees to display in command options
+    db.query(`SELECT * FROM employees`, function(err, empResponse) {
 
-        console.table(response);
-        var names = [];
-        for (var i = 0; i < response.length; i++) {
-            names.push(response[i].first_name + ' ' + response[i].last_name);
+        if (err !== null) {
+            console.log("Caught error: " + String(err));
+            return;
         }
 
+        console.table(empResponse);
+        var names = [];
+        for (var i = 0; i < empResponse.length; i++) {
+            // display name as 'first_name second_name'
+            names.push(empResponse[i].first_name + ' ' + empResponse[i].last_name);
+        }
+
+        // Get existing roles to display in command options
         db.query(`SELECT * FROM roles`, function(err, roleResponse) {
+
+            if (err !== null) {
+                console.log("Caught error: " + String(err));
+                return;
+            }
 
             var roles = [];
             for (var i = 0; i < roleResponse.length; i++) {
@@ -197,29 +297,31 @@ function updateEmployeeRole() {
 
             return inquirer
             .prompt(employeeUpdateQuestion)
-            .then((empResponse) => {
+            .then((inqEmpResponse) => {
+                // Find selected employee information
                 var emp = null;
 
-                for (var i = 0; i < response.length; i++) {
-                    if (response[i].first_name + ' ' + response[i].last_name === empResponse.employeeToUpdate) {
-                        emp = response[i];
+                for (var i = 0; i < empResponse.length; i++) {
+                    if (empResponse[i].first_name + ' ' + empResponse[i].last_name === inqEmpResponse.employeeToUpdate) {
+                        emp = empResponse[i];
                     }
                 }
-                console.log(emp);
             
+                // Find selected role information
                 var newRole = null;
                 for (var i = 0; i < roleResponse.length; i++) {
-                    if (roleResponse[i].title === empResponse.updatedEmployeeRole) {
+                    if (roleResponse[i].title === inqEmpResponse.updatedEmployeeRole) {
                         newRole = roleResponse[i];
                     }
                 }
             
+                // update existing employee with newly selected role
                 var dbQuery = `UPDATE employees 
                   SET title = "${newRole.title}", department = "${newRole.department}", salary = ${newRole.salary} 
                   WHERE first_name = "${emp.first_name}" AND last_name = "${emp.last_name}" AND manager="${emp.manager}"`;
                 console.log(dbQuery);
                 db.query(dbQuery, function(err, response) {
-
+                    console.log("Updated Employee's role");
                     return askUserForAction();
                 });
             });
@@ -227,85 +329,7 @@ function updateEmployeeRole() {
     });
 }
 
-function viewAllRoles() {
-    db.query(`SELECT * FROM roles`, function(err, response) {
-
-        console.table(response);
-        return askUserForAction();
+askUserForAction()
+    .catch((error) => {
+        console.log(error);
     });
-}
-
-function createRole() {
-    //Get all existing departments 
-    db.query(`SELECT * FROM departments`, function(err, response) {
-
-        var depts = [];
-        for (var i = 0; i < response.length; i++) {
-            depts.push(response[i].name);
-        }
-
-        var roleDept = [{
-            type: "list",
-            message: "Which department does the role belong to?",
-            choices: depts,
-            name: "departmentForRole",
-        }];
-
-        //'inquirer.prompt' for department info 
-        return inquirer
-        .prompt(roleQuestions.concat(roleDept))
-        .then((roleResponse) => {
-            var dbQuery =  `INSERT INTO roles (title, department, salary) VALUES (?, ?, ?)`;
-            db.query(dbQuery, 
-                [roleResponse.roleName, roleResponse.departmentForRole, roleResponse.roleSalary]
-                , function(err, res) {
-
-                return askUserForAction();
-            });
-        });
-    });
-
-
-}
-
-function viewAllDepartments() {
-    db.query(`SELECT * FROM departments`, function(err, response) {
-
-        console.table(response);
-        return askUserForAction();
-    });
-    
-}
-
-function createDepartment() {
-
-    //'inquirer.prompt' for department info 
-    return inquirer
-    .prompt(departmentQuestions)
-    .then((response) => {
-
-        var name = response.departmentName;
-
-        //THEN INSERT INTO department () VALUES
-        var dbQuery =  `INSERT INTO departments (name) VALUES (?)`;
-        db.query(dbQuery, name, function(err, response) {
-
-            return askUserForAction();
-        });
-
-    });
-}
-
-
-askUserForAction();
-    // .then(() => {
-    //     console.log("Exiting...");
-    // })
-    // .catch((error) => {
-    //     console.log(error);
-    //     if (error.isTtyError) {
-    //     //Prompt couldn't be rendered in the current environment 
-    //     } else {
-    //     //Something else went wrong
-    //     }
-    // });
